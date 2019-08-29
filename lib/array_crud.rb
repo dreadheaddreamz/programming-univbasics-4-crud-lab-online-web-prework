65e3f421bb100bde57066661b3dd55d4ab672615
James Batsell <jamesbatsell05@gmail.com>
2019-08-29 01:32:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array 
- favorite animals =["Dog,Cat,Shark,Lion"]
+ favorite animals = ["Dog","Cat" ,"Shark", "Lion"]
 end
 
 def add_element_to_end_of_array(array,element)
